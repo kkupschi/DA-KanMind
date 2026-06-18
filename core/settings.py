f5408207f907e71739c9e20e87d10c2141cf4326
kkupschi <kkupschi@gmail.com>
2026-06-18 10:29:46 +0200

    Add REST framework settings and Postman resource files for API collaboration

--- core/settings.py
+++ core/settings.py
@@ -123,3 +123,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 AUTH_USER_MODEL = "auth_app.User"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
